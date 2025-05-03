08aedfe9b7c845199267c7726253bb894d65c9e2
Sravanthi <sravanthierukulapati26@gmail.com>
2025-05-03 07:04:01 +0530

    Updated an url

--- game_summarizer/urls.py
+++ game_summarizer/urls.py
@@ -9,7 +9,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path('summarize/', views.summarize, name='summarize'),
-    path('analyze_existing/<int:game_id>/', views.analyze_existing_game, name='analyze_existing'),
-    path("game/<int:game_id>/analyze/", views.analyze_existing_game, name="analyze_existing_game")
+    path('analyze_existing/<int:game_id>/', views.analyze_existing_game, name='analyze_existing')
 ]
 
